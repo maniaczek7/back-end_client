e7045d44cee0e2092836e62995e09e6cdbf78de9
Radoslaw <maniaczek7@wp.pl>
2019-08-15 22:51:33 +0200

    update docker file to build on docker hub

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 # remember to click maven/gradle install to create JAR file!
 ./gradlew clean build
 eval $("C:\Program Files\Docker Toolbox\docker-machine.exe" env dev)
-docker build -t rmichniewskihome/application:back-end_login .
-docker push rmichniewskihome/application:back-end_login
-ssh ec2 docker pull rmichniewskihome/application:back-end_login
-ssh ec2 docker rm -f back-end_login
-ssh ec2 docker run -d --name=back-end_login -p 8082:8082 --rm rmichniewskihome/application:back-end_login
+docker build -t rmichniewskihome/back-end_login:latest .
+docker push rmichniewskihome/back-end_login:latest
+ssh ec2 docker pull rmichniewskihome/back-end_login:latest
+ssh ec2 docker rm -f back-end_login:latest
+ssh ec2 docker run -d --name=back-end_login -p 8082:8082 --rm rmichniewskihome/back-end_login:latest
 ssh ec2 docker ps
